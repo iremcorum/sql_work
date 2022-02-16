cc6684745660a74933dc280f4149dfa40cdb9605
Irem Corum <iremcorum@gmail.com>
2022-02-16 23:27:29 +0300

    best coding practices lecture

---
+++ coding_techniques_ch_7.sql
@@ -0,0 +1,18 @@
+use sales;
+
+CREATE TABLE IF NOT EXISTS test (
+    numbers 	INT(10),
+    words 		VARCHAR(10)
+);
+
+SELECT * FROM test;
+
+DROP TABLE test;
+
+/*
+Comment 1: Use forward slash-star for large comments.
+Comment 2
+*/
+
+# comment 3: use hashtage or double dash for small comments #
+-- comment 4 --
