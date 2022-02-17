b7dc191613895816d48b985aa8c7469be27e7277
Irem Corum <iremcorum@gmail.com>
2022-02-17 23:36:33 +0300

    new lectures for or, in, and logical order of sql

---
+++ ch_9_or.sql
@@ -0,0 +1,8 @@
+SELECT * FROM employees
+WHERE first_name = 'Denis' OR first_name = 'Elvis';
+
+SELECT * FROM employees
+WHERE first_name = 'Denis' AND first_name = 'Elvis';
+
+SELECT * FROM employees
+WHERE first_name = 'Kellie' OR first_name = 'Aruna';
