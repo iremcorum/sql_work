3ff7cbab79519ca566bcf695ac885b99c28791fd
Irem Corum <iremcorum@gmail.com>
2022-02-17 23:41:36 +0300

    edited the file

--- ch_5_first_steps.sql
+++ ch_5_first_steps.sql
@@ -6,17 +6,17 @@ CREATE TABLE sales
 (
 	purchase_number INT NOT NULL PRIMARY KEY auto_increment,
 	date_of_purchase DATE NOT NULL,
-    customer_id INT,
-    item_code VARCHAR(10) NOT NULL
-    );
+	customer_id INT,
+	item_code VARCHAR(10) NOT NULL
+);
 
 CREATE TABLE customers
 (
 	customer_id INT NOT NULL,
-    first_name VARCHAR(255),
-    last_name VARCHAR(255),
-    email_address VARCHAR(255),
-    number_of_complaints INT
+	first_name VARCHAR(255),
+	last_name VARCHAR(255),
+	email_address VARCHAR(255),
+	number_of_complaints INT
 );
 
 DROP TABLE sales;
