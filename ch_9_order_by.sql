970a748e3919bc6f92c57fa092900e3f2466f999
Irem Corum <iremcorum@gmail.com>
2022-02-23 17:38:23 +0300

    order by lecture

---
+++ ch_9_order_by.sql
@@ -0,0 +1,19 @@
+SELECT * FROM employees;
+
+SELECT * FROM employees
+ORDER BY first_name;
+
+SELECT * FROM employees
+ORDER BY first_name ASC;
+
+SELECT * FROM employees
+ORDER BY first_name DESC;
+
+SELECT * FROM employees
+ORDER BY emp_no DESC;
+
+SELECT * FROM employees
+ORDER BY first_name, last_name ASC;
+
+SELECT * FROM employees
+ORDER BY hire_date DESC;
