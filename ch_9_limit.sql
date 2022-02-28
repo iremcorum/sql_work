b6b2586edfaf28289db5237d341a82d8f3719947
Irem Corum <iremcorum@gmail.com>
2022-02-28 23:15:27 +0300

    limit lecture

---
+++ ch_9_limit.sql
@@ -0,0 +1,23 @@
+SELECT 
+    *
+FROM
+    salaries;
+    
+SELECT 
+    *
+FROM
+    salaries
+ORDER BY salary DESC;
+
+SELECT 
+    *
+FROM
+    salaries
+ORDER BY salary DESC
+LIMIT 10;
+
+SELECT 
+    *
+FROM
+    dept_emp
+LIMIT 100;
