b1052ce8418e828be7ac5b10a5b02eb3a8f1caec
Irem Corum <iremcorum@gmail.com>
2022-02-28 22:52:41 +0300

    having lecture

---
+++ ch_9_having.sql
@@ -0,0 +1,38 @@
+SELECT 
+    *
+FROM
+    employees
+WHERE
+    hire_date >= '2000-01-01';
+    
+SELECT 
+    *
+FROM
+    employees
+HAVING
+    hire_date >= '2000-01-01';
+
+SELECT 
+    first_name, COUNT(first_name) AS names_count
+FROM
+    employees
+WHERE
+    COUNT(first_name) > 250
+GROUP BY first_name
+ORDER BY first_name;
+
+SELECT 
+    first_name, COUNT(first_name) AS names_count
+FROM
+    employees
+GROUP BY first_name
+HAVING COUNT(first_name) > 250
+ORDER BY first_name;
+
+SELECT 
+    emp_no, AVG(salary)
+FROM
+    salaries
+GROUP BY emp_no
+HAVING AVG(salary) > 120000
+ORDER BY emp_no;
