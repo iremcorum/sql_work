62abe4a7e546f0bd4ecb65304eeecc87e2b267a1
Irem Corum <iremcorum@gmail.com>
2022-03-16 22:58:41 +0300

    min and max functions lecture

---
+++ ch_13_min_max.sql
@@ -0,0 +1,19 @@
+SELECT 
+    MAX(salary)
+FROM
+    salaries;
+
+SELECT 
+    MIN(salary)
+FROM
+    salaries;
+    
+SELECT 
+    MIN(emp_no)
+FROM
+    employees;
+    
+SELECT 
+    MAX(emp_no)
+FROM
+    employees;
