b7abc58d24d26589b71c44435c1e8e080cdbdc86
Irem Corum <iremcorum@gmail.com>
2022-04-08 18:13:27 +0300

    where - in subquery

---
+++ ch_15_sql_subquery_in.sql
@@ -0,0 +1,17 @@
+SELECT
+       *
+FROM dept_manager;
+
+SELECT
+       e.first_name, e.last_name
+FROM
+    employees e
+WHERE
+      e.emp_no IN (SELECT
+                          dm.emp_no
+      FROM
+           dept_manager dm);
+
+SELECT * FROM dept_manager
+WHERE emp_no IN (SELECT emp_no FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1995-01-01');
