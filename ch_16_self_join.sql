4d9e6ff69f0dce5dde4d5c23a452336d8fbf3f85
Irem Corum <iremcorum@gmail.com>
2022-04-13 18:15:13 +0300

    self join lecture

---
+++ ch_16_self_join.sql
@@ -0,0 +1,36 @@
+# Our task is from the emp_manager table, extract the record data only one of those
+# employees who are managers as well.
+SELECT * FROM emp_manager
+ORDER BY emp_manager.emp_no;
+
+SELECT
+       e1.*
+FROM emp_manager e1
+    JOIN emp_manager e2
+        ON e1.emp_no = e2.manager_no;
+
+SELECT
+       e2.*
+FROM emp_manager e1
+    JOIN emp_manager e2
+        ON e1.emp_no = e2.manager_no;
+
+SELECT
+       e1.emp_no, e1.dept_no, e2.manager_no
+FROM emp_manager e1
+    JOIN emp_manager e2
+        ON e1.emp_no = e2.manager_no;
+
+SELECT DISTINCT
+       e1.*
+FROM emp_manager e1
+    JOIN emp_manager e2
+        ON e1.emp_no = e2.manager_no;
+
+SELECT
+       e1.*
+FROM emp_manager e1
+    JOIN emp_manager e2
+        ON e1.emp_no = e2.manager_no
+WHERE e2.emp_no IN(SELECT
+    manager_no FROM emp_manager);
