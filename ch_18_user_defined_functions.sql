d19e9c8813b6854e3053431e84a4d56723087c06
Irem Corum <iremcorum@gmail.com>
2022-05-02 00:46:19 +0300

    user defined functions lecture and practice

---
+++ ch_18_user_defined_functions.sql
@@ -0,0 +1,47 @@
+USE employees;
+DROP FUNCTION IF EXISTS f_emp_avg_salary;
+
+DELIMITER $$
+CREATE FUNCTION f_emp_avg_salary(p_emp_no INT) RETURNS DECIMAL(10, 2)
+    DETERMINISTIC
+BEGIN
+    DECLARE v_avg_salary DECIMAL(10, 2);
+    SELECT AVG(s.salary) INTO v_avg_salary FROM employees e
+        JOIN salaries s ON e.emp_no = s.emp_no
+    WHERE e.emp_no = p_emp_no;
+
+    RETURN v_avg_salary;
+END $$
+
+DELIMITER ;
+
+SELECT f_emp_avg_salary(11300);
+
+DROP FUNCTION IF EXISTS f_emp_info;
+
+DELIMITER $$
+CREATE FUNCTION f_emp_info(p_first_name VARCHAR(255), p_last_name VARCHAR(255)) RETURNS DECIMAL(10, 2)
+DETERMINISTIC NO SQL READS SQL DATA
+BEGIN
+    DECLARE v_salary DECIMAL(10, 2);
+    DECLARE v_max_from_date DATE;
+
+    SELECT MAX(from_date) INTO v_max_from_date
+    FROM employees e
+    JOIN salaries s ON e.emp_no = s.emp_no
+    WHERE e.first_name = p_first_name AND e.last_name = p_last_name;
+
+    SELECT
+        s.salary INTO v_salary
+    FROM employees e
+    JOIN salaries s ON e.emp_no = s.emp_no
+    WHERE e.first_name = p_first_name
+    AND e.last_name = p_last_name
+    AND s.from_date = v_max_from_date;
+
+    RETURN v_salary;
+END $$
+
+DELIMITER ;
+
+SELECT f_emp_info('Aruna', 'Journel');
